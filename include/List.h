f364822fbb474c6480484d0348ce29bbbc9fe499
Matyasmester <matyas.zoboki@gmail.com>
2021-05-17 19:20:58 +0200

    Add UNION function, optimize others

--- include/List.h
+++ include/List.h
@@ -1,4 +1,6 @@
 #include <vector>
+#include <algorithm>
+#include <set>
 #ifndef LIST_H
 #define LIST_H
 #define EMPTY 0
@@ -135,26 +137,32 @@ class List
             }
         }
 
+        /** Dumps all elements of the List into a std::set<T>, then puts them back. **/
+        void RemoveDuplicates(){
+            set<T> Set;
+            unsigned short SetSize = Size;
+            for(int i = 0; i < SetSize; i++) Set.insert( _items[i] );
+            _items.assign(Set.begin(), Set.end());
+            UpdateSize();
+        }
+
         /** Returns the first element of the sequence. **/
         T First(){
             return _items[0];
-        }
-
-        /** Returns the element at the specified index.
-            Returns EMPTY (0) if the index is out of bounds. **/
-        T ElementAt(int i){
-            if(i > _items.capacity() || i < 0) return EMPTY;
-            return _items[i];
         }
 
         bool IsEmpty(){
             return _items.empty();
         }
 
-        /** Checks if the given item's index is -1. If so, it returns false, otherwise true. **/
+        /** Checks if the List contains the given item. **/
         bool Contains(T item){
-            if(IndexOf(item) == -1) return false;
-            return true;
+            return find(_items.begin(), _items.end(), item) != _items.end();
+        }
+
+        /** Determines if all the items are equal in the List. **/
+        bool All(){
+            return adjacent_find(_items.begin(), _items.end(), not_equal_to<T>()) == _items.end();
         }
 
         /** Determines if all the items in the List are the same as the given item. **/
@@ -163,6 +171,34 @@ class List
                 if(val != item) return false;
             }
             return true;
+        }
+
+        /** Takes a function that returns a bool, runs it on all items and determines if all returns true. **/
+        bool All(bool (*func)(T)){
+            for(T val : _items){
+                if(!func(val)) return false;
+            }
+            return true;
+        }
+
+        /** Takes a List and returns a List of elements that exist in both of them. **/
+        List<T> UNION(List<T> second){
+            List<T> Union;
+            for(int i = 0; i < Size; i++){
+                T current = _items[i];
+                for(int j = 0; j < second.GetSize(); j++){
+                    if(current == second[j]) Union.Add(current);
+                }
+            }
+            Union.RemoveDuplicates();
+            return Union;
+        }
+
+        /** Returns the element at the specified index.
+            Returns EMPTY (0) if the index is out of bounds. **/
+        T operator[](int i){
+            if(i > _items.capacity() || i < 0) return EMPTY;
+            return _items[i];
         }
 
 };
--- main.cpp
+++ main.cpp
@@ -4,37 +4,48 @@
 using namespace std;
 
 void Print(char val){
-    cout << val << endl;
+    cout << val << " ";
 }
 
 int main()
 {
     vector<char> arr = {'a', 'b', 'c', 'f', 'g', 'h', 'i', 'j', 'k', 'a', 'b', 'c', 'f', 'g', 'h', 'i', 'j', 'k', '4', '8', '9'};
-    List<char>* MyList = new List<char>();
-    MyList->Add('d');
-    MyList->Add('e');
-    MyList->Add(arr);
-    MyList->Add('2');
+    List<char> MyList;
+    MyList.Add('d');
+    MyList.Add('e');
+    MyList.Add(arr);
+    MyList.Add('2');
 
-    int Size = MyList->GetSize();
-    char First = MyList->First();
+    int Size = MyList.GetSize();
+    char First = MyList.First();
     int At = 6;
-    char Elem = MyList->ElementAt(At);
+    char Elem = MyList[At];
 
     cout << "First element: " << First << endl;
     cout << "Removing element at: " << At << ", which is: " << Elem << endl;
-    MyList->Remove(At);
-    cout << "New element at " << At << ": " << MyList->ElementAt(At) << endl;
-    cout << "Contains e: " << MyList->Contains('e') << endl;
-    cout << "Last index of j: " << MyList->LastIndexOf('j') << endl;
-    MyList->RemoveAll('a');
-    cout << "Contains a: " << MyList->Contains('a') << endl;
-    cout << "Are all the items c? : " << MyList->All('c')<< endl;
-    cout << "Count of j: " << MyList->Count('j')<< endl;
+    MyList.Remove(At);
+    cout << "New element at " << At << ": " << MyList[At] << endl;
+    cout << "Contains e: " << MyList.Contains('e') << endl;
+    cout << "Last index of j: " << MyList.LastIndexOf('j') << endl;
+    MyList.RemoveAll('a');
+    cout << "Contains a: " << MyList.Contains('a') << endl;
+    cout << "Are all the items c? : " << MyList.All('c')<< endl;
+    cout << "Are all items equal to each other? " << MyList.All()<< endl;
+    cout << "Count of j: " << MyList.Count('j')<< endl;
+    MyList.ForEach(Print);
+    cout << endl;
     cout << "Size: " << Size << endl;
-    MyList->ForEach(Print);
+    cout << "Removing duplicates." << endl;
+    MyList.RemoveDuplicates();
+    cout << "Size: " << MyList.GetSize() << endl;
 
-    delete MyList;
+    List<char> AnotherList;
+    AnotherList.Add(arr);
+    List<char> UNION = MyList.UNION(AnotherList);
+    cout << "Union of two lists:" << endl;
+    UNION.ForEach(Print);
+
+    delete &MyList;
 
     return 0;
 }
